d0aa18d07398f24593e9a9cea8e04ae2e71f238c
Bhavesh Wadhwani <bhaveshwadhwani21@gmail.com>
2021-07-17 20:37:52 +0530

    Finished

---
+++ client/build/precache-manifest.14cecf487d3081f75756849964291bae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd7a0e872d1c7d59d13c08ab0714e7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "914fcdca1fe6c256a53b",
+    "url": "/static/js/2.ed66238c.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/static/js/2.ed66238c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "573b90d64bbbb6add11d",
+    "url": "/static/js/main.cff435bb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
---
+++ client/src/components/Note.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import CreateIcon from "@material-ui/icons/Create";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addNote, removeNote, modifyNote, getNotes } from "../actions/notes";
+import { useEffect, Fragment, useState } from "react";
+import setAuthToken from "../utils/setAuthToken";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+import AddIcon from "@material-ui/icons/Add";
+import { toggleEdit } from "../actions/edit";
+
+const Note = ({ removeNote, getNotes, modifyNote, notes, toggleEdit }) => {
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    const get = async () => {
+      await getNotes();
+    };
+
+    get();
+  }, [getNotes]);
+  const [editMode, setEditMode] = useState(false);
+  const [note, setNote] = useState({
+    title: "",
+    text: "",
+  });
+  const [id, setId] = useState("");
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  };
+
+  const submitNote = (event) => {
+    setEditMode(false);
+    toggleEdit(editMode);
+    const _id = id;
+    modifyNote(note.title, note.text, _id);
+    setNote({
+      title: "",
+      text: "",
+    });
+
+    event.preventDefault();
+  };
+  const onDelete = (event) => {
+    removeNote(event.currentTarget.parentNode.getAttribute("data-key"));
+  };
+  const onEdit = (event) => {
+    setEditMode(true);
+    toggleEdit(editMode);
+    const key = event.currentTarget.parentNode.getAttribute("data-key");
+    setId(key);
+    const editNote = notes.find((item) => item._id === key);
+    const { title, text } = editNote;
+    setNote({
+      title,
+      text,
+    });
+    event.preventDefault();
+  };
+
+  if (notes) {
+    var list = notes.map((item) => {
+      return (
+        <div className="note" data-key={item._id} key={item._id}>
+          <h1>{item.title}</h1>
+          <p>{item.text}</p>
+          <button onClick={onDelete}>
+            <DeleteIcon />
+          </button>
+          {!editMode && (
+            <button onClick={onEdit}>
+              <CreateIcon />
+            </button>
+          )}
+        </div>
+      );
+    });
+  }
+  return (
+    <Fragment>
+      {editMode && (
+        <div className="edit-note">
+          <form className="create-note ">
+            <input
+              name="title"
+              onChange={handleChange}
+              value={note.title}
+              placeholder="Title"
+              rows={1}
+            />
+
+            <textarea
+              name="text"
+              onChange={handleChange}
+              value={note.text}
+              placeholder="Take a note..."
+              rows={3}
+            />
+            <Zoom in={true}>
+              <Fab onClick={submitNote}>
+                <AddIcon />
+              </Fab>
+            </Zoom>
+          </form>
+        </div>
+      )}
+      {list}
+    </Fragment>
+  );
+};
+Note.propTypes = {
+  removeNote: PropTypes.func.isRequired,
+  modifyNote: PropTypes.func.isRequired,
+  getNotes: PropTypes.func.isRequired,
+  notes: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  notes: state.notes.notes,
+});
+export default connect(mapStateToProps, {
+  addNote,
+  removeNote,
+  modifyNote,
+  getNotes,
+  toggleEdit,
+})(Note);
